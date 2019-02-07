e5abbe7993486ad854afd78d4c566378550dd051
Thomas Coutant <thomas.coutant@outlook.fr>
2019-02-07 08:42:31 +0100

    Creates routes and update package.json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
+import NotFound from './components/NotFound/NotFound';
+import Home from './components/Home/Home';
+
+import logo from './marvel-logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <header className="header">
+          <img src={logo} width="130" height="52" alt="Logo Marvel" />
+          <Navigation />
+        </header>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/comics" />
+          <Route path="/events" />
+          <Route path="/fan-zone" />
+
+          <Route path="/NotFound" component={NotFound} />
+          <Redirect to="/NotFound" />
+        </Switch>
       </div>
     );
   }
