1a95f645c9296e3ce7c5c55da0eba7bb12c07f56
Thomas Coutant <thomas.coutant@outlook.fr>
2019-02-13 21:28:53 +0100

    Adds crawling content on homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import './Home.css';
+
 class Home extends React.Component {
-    state = { heroes: {} };
-
-    componentDidMount() {
-
-        axios.get('https://api.nasa.gov/planetary/apod?api_key=1eooIq05BEbJ49FXBb4g4lUEBHVD3XWC6UmsRHNP')
-            .then(response => {
-                console.log(response.data);
-            })
-    }
 
     render() {
         return(
             <div>
+                <div className="fade"></div>
                 
+                <section className="star-wars">
+                    <div className="crawl">
+                    <div className="title">
+                        <p>Episode IV</p>
+                        <h1>A New Hope</h1>
+                    </div>
+                        
+                        <p>It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.</p>     
+                        <p>During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.</p>
+                        <p>Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…</p>
+                    </div>
+                </section>
             </div>
         );
     }
